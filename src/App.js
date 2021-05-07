22a7ff5e1e9d09923dca2d94035ee934179d555b
gnganapath <gnganapathi@gmail.com>
2021-05-08 00:19:26 +0530

    material-ui getting start links , materail-ui branch

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">            
+        Material UI getting Start 
+        <div><a><u>https://material-ui.com/getting-started/learn/</u></a> officeial site guide</div>
+        <div><u>https://material-ui.com/</u>   official page</div>
+        <div><u>https://www.youtube.com/watch?v=bL-ZwwF6wTc</u>  short Youtube video </div>
     </div>
   );
 }
