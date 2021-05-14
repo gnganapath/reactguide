3ac90c2f47d6b66a0c1482055ddd2aa52df47509
gnganapath <gnganapathi@gmail.com>
2021-05-14 21:38:11 +0530

    kick off - react formik

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         Material UI getting Start 
         <div><a><u>https://material-ui.com/getting-started/learn/</u></a> officeial site guide</div>
         <div><u>https://material-ui.com/</u>   official page</div>
-        <div><u>https://www.youtube.com/watch?v=bL-ZwwF6wTc</u>  short Youtube video </div>
+        <div><u>https://www.youtube.com/watch?v=bL-ZwwF6wTc</u>  short Youtube yvideo </div>
     </div>
   );
 }
